811b2eea0b6ff6f300656b23e1b667d37600f7ff
mstratiev <garmbrood@abv.bg>
2017-08-14 22:12:36 +0300

    custom symbols and dehumanize method

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 module.exports = (function() {
 
     var DEFAULT_SEPARATOR = '.';
+    var DEFAULT_DELIMINER = ',';
+    var DEFAULT_THOUSANDS = 'K';
+    var DEFAULT_MILLIONS = 'M';
 
     function NumabInstance(conf) {
         var configuration = {
-            separator: undefined
+            separator: undefined,
+            isCaseSensitive: false,
+            symbols: {
+                thousands: undefined,
+                millions: undefined
+            }
         };
 
         configure(conf);
 
         /**
          * Converts a string representation of a number.
-         * e.g. 2,3k to a number value 2300.
+         * e.g. 2.3k to a number value 2300
          * @param {string} str 
          * @return {number}
          */
@@ -21,7 +29,10 @@ module.exports = (function() {
             }
 
             // prepare and clean the string
-            var parsingString = str.toUpperCase().trim();
+            var parsingString = str.trim();
+            if (!configuration.isCaseSensitive) {
+                parsingString = parsingString.toUpperCase();
+            }
 
             if (parsingString == '') {
                 // returns NaN if string is empty or was composed only of spaces
@@ -29,16 +40,6 @@ module.exports = (function() {
             }
             var millions, thousands;
             var results = 0;
-
-            // preparse
-            switch (true) {
-                case (parsingString.indexOf('M') > -1):
-                    millions = true;
-                    break;
-                case (parsingString.indexOf('K') > -1):
-                    thousands = true;
-                    break;
-            }
 
             // replace separator with new custom one
             if (configuration.separator) {
@@ -49,19 +50,49 @@ module.exports = (function() {
                 parsingString = parsingString.replace(configuration.separator, DEFAULT_SEPARATOR);
             }
 
+            if (configuration.symbols.millions) {
+                parsingString = parsingString.replace(configuration.symbols.millions, DEFAULT_MILLIONS);
+            }
+
+            if (configuration.symbols.thousands) {
+                parsingString = parsingString.replace(configuration.symbols.thousands, DEFAULT_THOUSANDS);
+            }
+
+            // preparse
+            switch (true) {
+                case (parsingString.indexOf(DEFAULT_MILLIONS) > -1):
+                    millions = true;
+                    break;
+                case (parsingString.indexOf(DEFAULT_THOUSANDS) > -1):
+                    thousands = true;
+                    break;
+            }
+
             // parse string number
             var tempArr;
             if (millions) {
-                tempArr = parsingString.split('M');
+                tempArr = parsingString.split(DEFAULT_MILLIONS);
                 results = tempArr[0] * 1000000;
             } else if (thousands) {
-                tempArr = parsingString.split('K');
+                tempArr = parsingString.split(DEFAULT_THOUSANDS);
                 results = tempArr[0] * 1000;
             } else {
                 results = str * 1;
             }
             return results;
-        };
+        }
+
+        /**
+         * Converts a "humanized" string representation of number to machine-readable number.
+         * e.g. "1.000" or "1,000" to a number value 1000
+         * @param {string} str The string that is to be parsed.
+         */
+        function dehumanize(str) {
+            var strArr = str.split(configuration.separator ? configuration.separator : DEFAULT_SEPARATOR);
+            var number = strArr[0] ? strArr[0].split(configuration.delimiter ? configuration.delimiter : DEFAULT_DELIMINER).join('') * 1 : 0;
+            var decimal = strArr[1] ? strArr[1] * 1 / (strArr[1].length * 10) : 0;
+            return number + decimal;
+        }
 
         /**
          * Configures the Numab instance.
@@ -69,10 +100,27 @@ module.exports = (function() {
          * @param {any} conf Configuration object for the numab.
          */
         function configure(conf) {
-            if (conf) {
+            if (!conf) return;
+            if (conf.separator) {
                 configuration.separator = conf.separator;
             }
-        };
+            if (conf.symbols && !conf.isCaseSensitive) {
+                if (conf.symbols.thousands) {
+                    configuration.symbols.thousands = conf.symbols.thousands.toUpperCase();
+                }
+                if (conf.symbols.millions) {
+                    configuration.symbols.millions = conf.symbols.millions.toUpperCase();
+                }
+            } else if (conf.symbols && conf.isCaseSensitive) {
+                configuration.isCaseSensitive = conf.isCaseSensitive;
+                if (conf.symbols.thousands) {
+                    configuration.symbols.thousands = conf.symbols.thousands;
+                }
+                if (conf.symbols.millions) {
+                    configuration.symbols.millions = conf.symbols.millions;
+                }
+            }
+        }
 
         /**
          * Creates a separate instance of Numab.
@@ -85,9 +133,15 @@ module.exports = (function() {
         return {
             createInstance: createInstance,
             parse: parse,
+            dehumanize: dehumanize,
             config: configure
         };
     }
 
     return new NumabInstance();
 })();
+
+
+a = module.exports.parse('   ');
+
+console.log(a)
--- test/test.js
+++ test/test.js
@@ -11,86 +11,143 @@ describe('numab', function() {
             assert.ok(numab.createInstance());
         });
 
-        it('should return a number when passed "2" (string number) ', function() {
-            var actual = numab.parse('2');
-            assert(typeof(actual) == 'number');
+        describe('parse', function() {
+
+            it('should return a number when passed "2" (string number) ', function() {
+                var actual = numab.parse('2');
+                assert(typeof(actual) == 'number');
+            });
+
+            describe('should return NaN when passed anything else than a number-like string', function() {
+                it('is passed empty string', function() {
+                    assert(isNaN(numab.parse(''), 'empty string'));
+                });
+
+                it('is passed spaces', function() {
+                    assert(isNaN(numab.parse('  '), 'spaces'));
+                });
+
+                it('is passed "a"', function() {
+                    assert(isNaN(numab.parse('a'), 'simple string'));
+                });
+
+                it('is passed "a k"', function() {
+                    assert(isNaN(numab.parse('a k'), 'string containing abbr character'));
+                });
+
+                it('is passed {}', function() {
+                    assert(isNaN(numab.parse({}), 'object'));
+                });
+            });
+
+            describe('default separator .', function() {
+                it('should return 250 when passes "0.25k"', function() {
+                    assert.equal(numab.parse('0.25k'), 250);
+                });
+
+                it('should return 250 when passed "0.25 K"', function() {
+                    assert.equal(numab.parse('0.25k'), 250);
+                });
+
+                it('should return 250 when passed " 0.25   K"', function() {
+                    assert.equal(numab.parse('0.25k'), 250);
+                });
+            });
+
+            describe('custom separator ,,', function() {
+                var num = numab.createInstance();
+                num.config({ separator: ',,' });
+
+                it('should return 250 when passed "0,,25k"', function() {
+                    assert.equal(num.parse('0,,25k'), 250);
+                });
+
+                it('should return 2000 when passed "2000"', function() {
+                    assert.equal(num.parse('2000'), 2000);
+                });
+
+                it('should return NaN when passed default separator', function() {
+                    assert(Number.isNaN(num.parse('2.5k')));
+                });
+
+                it('should apply custom separator to instance', function() {
+                    var i = numab.createInstance();
+                    i.config({ separator: ",," });
+                    var actual = i.parse('2,,5k');
+                    assert.equal(actual, 2500);
+                });
+            });
+
+            describe('custom separator ..', function() {
+                var num = numab.createInstance();
+                num.config({ separator: '..' });
+
+                it('should return 250 when passed "0..25k"', function() {
+                    assert.equal(num.parse('0..25k'), 250);
+                });
+
+                it('should return 2000 when passed "2000"', function() {
+                    assert.equal(num.parse('2000'), 2000);
+                });
+
+                it('should return NaN when passed default separator', function() {
+                    assert(Number.isNaN(num.parse('2.5k')));
+                });
+            });
+
+            describe('custom symbols', function() {
+                var num = numab.createInstance();
+                num.config({
+                    symbols: {
+                        thousands: 'hil',
+                        millions: 'Millions'
+                    }
+                });
+                it('should return 2 when passed "2"', function() {
+                    assert.equal(num.parse("2"), 2);
+                });
+
+                it('should return 200 when passed "0.2 hil"', function() {
+                    assert.equal(num.parse('0.2 hil'), 200);
+                });
+
+                it('should return 500000 when passed 0.5 Millions', function() {
+                    assert.equal(num.parse('0.5 Millions'), 500000);
+                });
+            });
+
+            describe('original instance and clone should return same values', function() {
+                it('should return same values with different separators', function() {
+                    var expected = 2500;
+                    var actualGlobal = numab.parse("2.5k");
+                    var actualInstance = numab.createInstance({ separator: ",," }).parse('2,,5k');
+                    assert.equal(actualGlobal, expected);
+                    assert.equal(actualInstance, expected);
+                    assert.equal(actualInstance, actualGlobal);
+                });
+            });
         });
 
-        it('should return NaN when passed anything else than a number-like string', function() {
-            assert(isNaN(numab.parse(''), 'empty string'));
-            assert(isNaN(numab.parse('  '), 'spaces'));
-            assert(isNaN(numab.parse('a'), 'simple string'));
-            assert(isNaN(numab.parse('a k'), 'string containing abbr character'));
-            assert(isNaN(numab.parse({}), 'object'));
-        });
+        describe('dehumanize', function() {
+            describe('sanity', function() {
+                it('should return NaN when passed non number', function() {
+                    assert(isNaN(numab.dehumanize('asd')));
+                });
+            });
 
+            describe('default case', function() {
+                itShouldDehumanize('1.2', 1.2);
+                itShouldDehumanize('1,000', 1000);
+                itShouldDehumanize('1,000,100.1', 1000100.1);
+            });
+        });
     });
 
-    describe('default separator .', function() {
-        it('should return 250 when passes "0.25k"', function() {
-            assert.equal(numab.parse('0.25k'), 250);
+    function itShouldDehumanize(passed, expected) {
+        it('shold return ' + expected + ' when passed "' + passed + '"', function() {
+            assert.equal(numab.dehumanize(passed), expected);
         });
-
-        it('should return 250 when passed "0.25 K"', function() {
-            assert.equal(numab.parse('0.25k'), 250);
-        });
-
-        it('should return 250 when passed " 0.25   K"', function() {
-            assert.equal(numab.parse('0.25k'), 250);
-        });
-    });
-
-    describe('custom separator ,,', function() {
-        var num = numab.createInstance();
-        num.config({ separator: ',,' });
-
-        it('should return 250 when passed "0,,25k"', function() {
-            assert.equal(num.parse('0,,25k'), 250);
-        });
-
-        it('should return 2000 when passed "2000"', function() {
-            assert.equal(num.parse('2000'), 2000);
-        });
-
-        it('should return NaN when passed default separator', function() {
-            assert(Number.isNaN(num.parse('2.5k')));
-        });
-
-        it('should apply custom separator to instance', function() {
-            var i = numab.createInstance();
-            i.config({ separator: ",," });
-            var actual = i.parse('2,,5k');
-            assert.equal(actual, 2500);
-        });
-    });
-
-    describe('custom separator ..', function() {
-        var num = numab.createInstance();
-        num.config({ separator: '..' });
-
-        it('should return 250 when passed "0..25k"', function() {
-            assert.equal(num.parse('0..25k'), 250);
-        });
-
-        it('should return 2000 when passed "2000"', function() {
-            assert.equal(num.parse('2000'), 2000);
-        });
-
-        it('should return NaN when passed default separator', function() {
-            assert(Number.isNaN(num.parse('2.5k')));
-        });
-    });
-
-    describe('original instance and clone should return same values', function() {
-        it('should return same values with different separators', function() {
-            var expected = 2500;
-            var actualGlobal = numab.parse("2.5k");
-            var actualInstance = numab.createInstance({ separator: ",," }).parse('2,,5k');
-            assert.equal(actualGlobal, expected);
-            assert.equal(actualInstance, expected);
-            assert.equal(actualInstance, actualGlobal);
-        });
-    });
+    }
 
     function isNaN(a) {
         return Number.isNaN(a);
